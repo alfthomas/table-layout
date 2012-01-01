61cc6ef0b5cab544ff0322caf4b7380ae9df883e
alfthomas <alf.thomas.nilsen@gmail.com>
2012-01-01 20:25:15 +0100

    Set up a simple table

--- src/net/alfthomas/android/DemoTableLayout.java
+++ src/net/alfthomas/android/DemoTableLayout.java
@@ -2,12 +2,29 @@ package net.alfthomas.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
 
 public class DemoTableLayout extends Activity {
-    /** Called when the activity is first created. */
+    
+    private TableLayout layout;
+    private Button button;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        layout = (TableLayout) findViewById(R.id.tableLayout1);
+        button = (Button) findViewById(R.id.collapse);
+    }
+    
+    public void toggleHiddenRows(View view) {
+        layout.setColumnCollapsed(1, !layout.isColumnCollapsed(1));
+        if (layout.isColumnCollapsed(1)) {
+            button.setText("Show second column");
+        } else {
+            button.setText("Hide second column");
+        }
     }
 }
